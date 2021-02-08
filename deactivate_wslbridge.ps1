572435503622eda3bac389600972fe40ae0195bd
robertjbush <robjbush@gmail.com>
2021-02-08 13:11:04 +0000

    Modified scripts to get-internetsharing status after runtime and to set script restriction policy to RemoteSigned for CurrentUser

--- deactivate_wslbridge.ps1
+++ deactivate_wslbridge.ps1
@@ -1,7 +1,8 @@
-set-ExecutionPolicy Unrestricted
-Import-module .\MrInternetConnectionSharing.ps1 -Force
+#set-ExecutionPolicy Unrestricted
+Import-module "C:\Users\Robert Bush\Desktop\MrInternetConnectionSharing.ps1" -Force
 Set-MrInternetConnectionSharing -InternetInterfaceName "WiFi" -LocalInterfaceName "vEthernet (WSL) 2" -Enabled $false
+Get-MrInternetConnectionSharing -InternetInterfaceName "vEthernet (WSL) 2", "WiFi" 
 Start-Sleep -s 1
 Set-VMSwitch WSL -SwitchType Internal
 wsl --shutdown
-set-ExecutionPolicy Restricted
+#set-ExecutionPolicy Restricted
--- wslbridge.ps1
+++ wslbridge.ps1
@@ -1,10 +1,10 @@
-set-ExecutionPolicy Unrestricted
+#set-ExecutionPolicy Unrestricted
 Set-NetConnectionProfile -Name "Unidentified network" -NetworkCategory Private
 Set-VMSwitch WSL -NetAdapterName Ethernet
 Start-Sleep -s 1
 wsl --user root /home/aa-ros/network-config.sh
-Import-module .\MrInternetConnectionSharing.ps1 -Force
-Get-MrInternetConnectionSharing -InternetInterfaceName "vEthernet (WSL) 2", "WiFi" 
+Import-module "C:\Users\Robert Bush\Desktop\MrInternetConnectionSharing.ps1" -Force
 Set-MrInternetConnectionSharing -InternetInterfaceName "WiFi" -LocalInterfaceName "vEthernet (WSL) 2" -Enabled $true
+Get-MrInternetConnectionSharing -InternetInterfaceName "vEthernet (WSL) 2", "WiFi" 
 Start-Sleep -s 1
-set-ExecutionPolicy Restricted
+#set-ExecutionPolicy Restricted
