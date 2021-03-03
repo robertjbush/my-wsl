bd42bd139657fc13fcaa21762049b296f40bd162
robertjbush <robert@additiveautomations.com>
2021-03-03 11:51:58 +0000

    Changed paths to reflect new username in windows

--- wslbridge.ps1
+++ wslbridge.ps1
@@ -2,8 +2,8 @@
 Set-NetConnectionProfile -Name "Unidentified network" -NetworkCategory Private
 Set-VMSwitch WSL -NetAdapterName Ethernet
 Start-Sleep -s 1
-wsl --user root /home/aa-ros/network-config.sh
-Import-module "C:\Users\Robert Bush\Desktop\MrInternetConnectionSharing.ps1" -Force
+wsl --user root /home/rob/network-config.sh
+Import-module "C:\Users\rob\Desktop\MrInternetConnectionSharing.ps1" -Force
 Set-MrInternetConnectionSharing -InternetInterfaceName "WiFi" -LocalInterfaceName "vEthernet (WSL) 2" -Enabled $true
 Get-MrInternetConnectionSharing -InternetInterfaceName "vEthernet (WSL) 2", "WiFi" 
 Start-Sleep -s 3
